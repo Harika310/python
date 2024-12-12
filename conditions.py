c4f49f8e0c67d684a2043b99ea1156b0359708e6
Harika Eravathri <harikaeravathri@gmail.com>
2024-12-12 10:47:58 -0800

    python

--- conditions.py
+++ conditions.py
@@ -1,5 +1,16 @@
-num1 = 20
-if num1 < = 50:
-    print("number is lessthan 50")
-    else:
-        print("number is greaterthan 50")
+num1 = 100
+if (num1 >= 50) and (num1 < 200):
+    print("number is greater than or equal to 50 and less than 200")
+elif num1 <= 50: # elif: else if
+    print("Number is less than or equal to 50")    
+else:
+    print("number is greater than 50")
+
+if True:
+    print("I am evaluated")
+if -1:
+    print("I am evaluated")
+if 0:
+    print("I am evaluated")
+else:
+    print("I am not evaluated")
