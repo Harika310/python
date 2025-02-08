2ef9a4c7335f846568b0a190676100f58281cb62
Harika Eravathri <harikaeravathri@gmail.com>
2025-02-08 12:37:22 -0800

    python

--- ec2.py
+++ ec2.py
@@ -1,16 +1,39 @@
 import boto3  # pip install boto3
 
-ec2 = boto3.client('ec2')
+# Initialize a session using Amazon EC2
+ec2 = boto3.resource('ec2')
 
-def list_instances():
-    response = ec2.describe_instances()
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            print(f"Instance ID: {instance['InstanceId']}")
+# Create a new EC2 instance
+instances = ec2.create_instances(
+    ImageId='ami-09c813fb71547fc4f',  # Replace with your desired AMI ID
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    Vpc_id=["sg-089040169ad25fd55"]
+    # KeyName='your-key-pair-name'  # Replace with your key pair name
+)
 
+# Print instance details
+for instance in instances:
+    print(f"EC2 Instance ID: {instance.id}")
+    print(f"Instance State: {instance.state['Name']}")
 
-if __name__ == "__main__":
-    list_instances()
+# ec2 = boto3.client('ec2')
+
+# def list_instances():
+#     response = ec2.describe_instances()
+#     for reservation in response['Reservations']:
+#         for instance in reservation['Instances']:
+#             print(f"Instance ID: {instance['InstanceId']}")
+
+# def start_instance(instance_id):
+#     ec2.start_instances(InstanceIds=[instance_id])
+#     print(f"Started instance: {instance_id}")
+
+# if __name__ == "__main__":
+#     list_instances()
+#     start_instance('i-0abcd1234efgh5678')
+
 
 # import boto3
 
